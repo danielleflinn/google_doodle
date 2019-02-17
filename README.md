5ecf5c35e0ccd36cb4b2b3e16d6879a4359972c7
Danielle Flinn <danielleflinn29@gmail.com>
2019-02-16 20:31:38 -0800

    Added main content
    html, css, js
    copied from codepen workspace

--- README.md
+++ README.md
@@ -9,5 +9,8 @@ Winter 2019 Web 2 Starter. Our DIY framework.
 * [Animate CSS](https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css)
 * [Google Styling](https://codepen.io/manikoth/pen/NNNBYe.css)
 
+## JS Plugins 
+* [Google Behavior](https://codepen.io/manikoth/pen/XpPMXv.js)
+
 ### Adapted From
 * [Wave Animation](https://codepen.io/linktothapast/pen/vKQJkE)
--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,31 @@
 console.log('OK');
 
 // Your Awesome Scripts
+console.log("Cafe coffee is coffeelicious");
+
+$('.cafe').lettering();
+
+var clicks = 0;
+
+$('#pour-button').click(function() {
+   
+  
+  if (clicks == 0){ //first click
+        $("#pour-button").text("Pour!");
+        $('.cafe-wrapper').addClass('cafe-move');
+      $('.mug-wrapper').addClass('mug-slide');
+    } else{
+      if ($("#pour-button").text() == "Pour!") {
+        $("#pour-button").text("Empty");
+      } else {
+        $("#pour-button").text("Pour!");
+      }
+
+      
+        $('.pot-wave').toggleClass('wave-pour');
+        $('#pot').toggleClass('pour');
+        $('.wave-container').toggleClass('move');
+        $('.wave-container').addClass('move-back');
+    }
+    ++clicks;
+});
